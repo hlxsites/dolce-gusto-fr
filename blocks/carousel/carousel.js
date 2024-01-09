0253c02040a06b2a2c0b3758ba713adc4ec682e7
Hannes Hertach <hanneshertach@gmail.com>
2024-01-09 11:08:38 +0100

    add carousel controls

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,5 +1,20 @@
 import { loadCSS } from '../../scripts/aem.js';
 
+function setActiveButton(activeSlideNumber, buttons) {
+  buttons.forEach((button) => button.classList.remove('active'));
+  buttons[activeSlideNumber].classList.add('active');
+}
+
+function goToSlide(slideNumber, slidesContainer, slides, buttons) {
+  slidesContainer.scrollTo(slideNumber * slides[0].clientWidth, 0);
+  setActiveButton(slideNumber, buttons);
+}
+
+function findActiveSlide(slidesContainer, slides) {
+  const activeElement = slidesContainer.querySelector('.active');
+  return slides.indexOf(activeElement);
+}
+
 export async function createCarousel(...slides) {
   await loadCSS('/blocks/carousel/carousel.css');
 
@@ -24,6 +39,35 @@ export async function createCarousel(...slides) {
       </div>
     </div>
   `);
+
+  const selectorButtons = [...overlay.querySelectorAll('.selector-buttons > button')];
+
+  slidesContainer.addEventListener('scroll', () => {
+    const activeElement = Math.round(slidesContainer.scrollLeft / slides[0].clientWidth);
+    if (!slides[activeElement].classList.contains('active')) {
+      slides.forEach((slide) => slide.classList.remove('active'));
+      slides[activeElement].classList.add('active');
+      setActiveButton(activeElement, selectorButtons);
+    }
+  }, { passive: true });
+
+  overlay.querySelectorAll('.selector-buttons > button').forEach((button, i, buttons) => {
+    button.addEventListener('click', () => {
+      goToSlide(i, slidesContainer, slides, buttons);
+    });
+  });
+
+  overlay.querySelector('button[name="previous"]').addEventListener('click', () => {
+    const currentSlide = findActiveSlide(slidesContainer, slides);
+    const nextSlide = currentSlide > 0 ? currentSlide - 1 : slides.length - 1;
+    goToSlide(nextSlide, slidesContainer, slides, selectorButtons);
+  });
+
+  overlay.querySelector('button[name="next"]').addEventListener('click', () => {
+    const currentSlide = findActiveSlide(slidesContainer, slides);
+    const nextSlide = currentSlide < slides.length - 1 ? currentSlide + 1 : 0;
+    goToSlide(nextSlide, slidesContainer, slides, selectorButtons);
+  });
 
   carouselRoot.append(overlay);
   carouselRoot.append(slidesContainer);
